58a1974a147721ccd0fc23618f881eb2d44cc8fa
johache <jacquesolivier.hache@gmail.com>
2014-05-07 12:55:43 +0800

    STYLE on GUM files

--- samples/web/content/getusermedia/js/main.js
+++ samples/web/content/getusermedia/js/main.js
@@ -1,23 +1,16 @@
 // variables in global scope so available to console
 video = document.querySelector("video");
 constraints = {audio: false, video: true};
-// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
 function successCallback(stream){
   window.stream = stream; // stream available to console
-  // if (window.URL) {
-  //   video.src = window.URL.createObjectURL(stream);
-  // } else {
-  //   video.src = stream;
-  // }
-
-  video = attachMediaStream(video, stream);
+	video = attachMediaStream(video, stream);
 }
 
 function errorCallback(error){
   console.log("navigator.getUserMedia error: ", error);
 }
 
-  function TemPluginLoaded() {
-	navigator.getUserMedia(constraints, successCallback, errorCallback);
-  }
+function TemPluginLoaded() {
+navigator.getUserMedia(constraints, successCallback, errorCallback);
+}
